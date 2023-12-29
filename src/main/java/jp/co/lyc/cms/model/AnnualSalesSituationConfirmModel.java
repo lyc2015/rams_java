c10da4dfa17476491446dbc7404a2c1f512fff79
ChengYaoYan <yyc298520@gmail.com>
2023-12-29 15:09:14 +0900

    fix: 客户名称显示略写

--- src/main/java/jp/co/lyc/cms/model/AnnualSalesSituationConfirmModel.java
+++ src/main/java/jp/co/lyc/cms/model/AnnualSalesSituationConfirmModel.java
@@ -3,86 +3,86 @@ package jp.co.lyc.cms.model;
 import java.io.Serializable;
 
 public class AnnualSalesSituationConfirmModel implements Serializable {
-    String employeeNo;
-    String salesYearAndMonth;
-    String salesStaff;
-    String employeeFirstName;
-    String employeeLastName;
-    String salesStaffFirstName;
-    String salesStaffLastName;
-    String customerNo;
-    String admissionEndDate;
-    String customerName;
+  String employeeNo;
+  String salesYearAndMonth;
+  String salesStaff;
+  String employeeFirstName;
+  String employeeLastName;
+  String salesStaffFirstName;
+  String salesStaffLastName;
+  String customerNo;
+  String admissionEndDate;
+  String customerAbbreviation;
 
-    public String getEmployeeNo() {
-      return employeeNo;
-    }
+  public String getEmployeeNo() {
+    return employeeNo;
+  }
 
-    public void setEmployeeNo(String employeeNo) {
-      this.employeeNo = employeeNo;
-    }
+  public void setEmployeeNo(String employeeNo) {
+    this.employeeNo = employeeNo;
+  }
 
-    public String getSalesYearAndMonth() {
-      return salesYearAndMonth;
-    }
+  public String getSalesYearAndMonth() {
+    return salesYearAndMonth;
+  }
 
-    public void setSalesYearAndMonth(String salesYearAndMonth) {
-      this.salesYearAndMonth = salesYearAndMonth;
-    }
+  public void setSalesYearAndMonth(String salesYearAndMonth) {
+    this.salesYearAndMonth = salesYearAndMonth;
+  }
 
-    public String getSalesStaff() {
-      return salesStaff;
-    }
+  public String getSalesStaff() {
+    return salesStaff;
+  }
 
-    public void setSalesStaff(String salesStaff) {
-      this.salesStaff = salesStaff;
-    }
+  public void setSalesStaff(String salesStaff) {
+    this.salesStaff = salesStaff;
+  }
 
-    public String getEmployeeFirstName() {
-      return employeeFirstName;
-    }
+  public String getEmployeeFirstName() {
+    return employeeFirstName;
+  }
 
-    public void setEmployeeFirstName(String employeeFirstName) {
-      this.employeeFirstName = employeeFirstName;
-    }
+  public void setEmployeeFirstName(String employeeFirstName) {
+    this.employeeFirstName = employeeFirstName;
+  }
 
-    public String getEmployeeLastName() {
-      return employeeLastName;
-    }
+  public String getEmployeeLastName() {
+    return employeeLastName;
+  }
 
-    public void setEmployeeLastName(String employeeLastName) {
-      this.employeeLastName = employeeLastName;
-    }
+  public void setEmployeeLastName(String employeeLastName) {
+    this.employeeLastName = employeeLastName;
+  }
 
-    public String getSalesStaffFirstName() {
-      return salesStaffFirstName;
-    }
+  public String getSalesStaffFirstName() {
+    return salesStaffFirstName;
+  }
 
-    public void setSalesStaffLastName(String salesStaffLastName) {
-      this.salesStaffLastName = salesStaffLastName;
-    }
+  public void setSalesStaffLastName(String salesStaffLastName) {
+    this.salesStaffLastName = salesStaffLastName;
+  }
 
-    public String getCustomerNo() {
-      return customerNo;
-    }
+  public String getCustomerNo() {
+    return customerNo;
+  }
 
-    public void setCustomerNo(String customerNo) {
-      this.customerNo = customerNo;
-    }
+  public void setCustomerNo(String customerNo) {
+    this.customerNo = customerNo;
+  }
 
-    public String getAdmissionEndDate() {
-      return admissionEndDate;
-    }
+  public String getAdmissionEndDate() {
+    return admissionEndDate;
+  }
 
-    public void setAdmissionEndDate(String admissionEndDate) {
-      this.admissionEndDate = admissionEndDate;
-    }
+  public void setAdmissionEndDate(String admissionEndDate) {
+    this.admissionEndDate = admissionEndDate;
+  }
 
-    public String getCustomerName() {
-      return customerName;
-    }
+  public String getCustomerAbbreviation() {
+    return customerAbbreviation;
+  }
 
-    public void setCustomerName(String customerName) {
-      this.customerName = customerName;
-    }
+  public void setCustomerAbbreviation(String customerAbbreviation) {
+    this.customerAbbreviation = customerAbbreviation;
+  }
 }
--- src/main/java/jp/co/lyc/cms/service/AnnualSalesSituationConfirmService.java
+++ src/main/java/jp/co/lyc/cms/service/AnnualSalesSituationConfirmService.java
@@ -1,6 +1,5 @@
 package jp.co.lyc.cms.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
